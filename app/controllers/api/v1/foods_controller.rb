8e20192394131d645bace203dba1f7853db1969b
jtrtj <jt.r@live.com>
2018-10-01 16:39:45 -0600

    build index method for foods controller

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::FoodsController < ApplicationController
+  def index
+    foods = FoodsPresenter.new.all
+    render json:   foods.to_json, 
+           status: foods.message
+  end
 end
