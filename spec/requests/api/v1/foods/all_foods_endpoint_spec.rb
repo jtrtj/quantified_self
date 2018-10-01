a748c5bf964951a733c0eb282763ebcfe393463d
jtrtj <jt.r@live.com>
2018-10-01 16:19:23 -0600

    add faker, generate food model

--- spec/requests/api/v1/foods/all_foods_endpoint_spec.rb
+++ spec/requests/api/v1/foods/all_foods_endpoint_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 describe '/api/v1' do
   context  'GET /foods' do
     it 'returns a json object with all foods in the database' do
-    
+      banana = Food.create(name: 'Banana', calories: 150)
+      cheetos = Food.create(name: 'Cheetos', calories: 200)
+
+      get '/api/v1/foods'
+      result = JSON.parse(response.body)
+
+      expect(result.count).to eq(2)
+      expect(result.first).to have_key("name") 
+      expect(result.first).to have_key("calories") 
     end
   end 
 end
