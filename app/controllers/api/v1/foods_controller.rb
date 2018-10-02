173bf8abce357078d489a4f108f52c9b2cc799ab
jtrtj <jt.r@live.com>
2018-10-02 13:48:18 -0600

    add actions to show method in food controller

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::FoodsController < ApplicationController
   def index
     render json: FoodsPresenter.jsonable_hash
   end
+
+  def show
+    food_presenter = FoodPresenter.new(food_params)
+    render json:   food_presenter.jsonable_hash,
+           status: food_presenter.message
+  end
 end
