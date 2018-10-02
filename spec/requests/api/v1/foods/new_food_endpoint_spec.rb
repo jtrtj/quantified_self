c1bfd2c21811b8820be00936c205f683e77cf07a
jtrtj <jt.r@live.com>
2018-10-02 17:05:32 -0600

    all tests green

--- spec/requests/api/v1/foods/new_food_endpoint_spec.rb
+++ spec/requests/api/v1/foods/new_food_endpoint_spec.rb
@@ -9,12 +9,12 @@ describe '/api/v1' do
 
       post '/api/v1/foods', params: parameters
       status = response.status
-      response = JSON.parse(response.body)
-
+      result = JSON.parse(response.body)
+      # byebug
       expect(status).to eq(200)
-      expect(response).to have_key(:id)
-      expect(response[:"name"]).to eq(parameters[:"food"][:"name"])
-      expect(response[:"calories"]).to eq(parameters[:"food"][:"calories"])
+      expect(result).to have_key("id")
+      expect(result["name"]).to eq(parameters[:"food"][:"name"])
+      expect(result["calories"]).to eq(parameters[:"food"][:"calories"].to_i)
     end
   end
 end
