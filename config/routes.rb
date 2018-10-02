de76de14f65aed7f156f8300b669ae1032788f88
jtrtj <jt.r@live.com>
2018-10-02 15:43:02 -0600

    add route for new food

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/foods', to: 'foods#index'
-      get 'foods/:id', to: 'foods#show'
+      get '/foods/:id', to: 'foods#show'
+      post '/foods', to: 'foods#create'
     end
   end
 end
