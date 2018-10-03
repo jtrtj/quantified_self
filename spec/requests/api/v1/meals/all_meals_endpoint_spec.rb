3beb2e5faa8622bb5dfed06cff526de555d8db08
jtrtj <jt.r@live.com>
2018-10-03 13:45:43 -0600

    write first test for meals endpoint

---
+++ spec/requests/api/v1/meals/all_meals_endpoint_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe '/api/v1' do
+  context 'GET /meals' do
+    it 'will return all the meals in the database along with their foods' do
+      meals = create_list(:meal, 3)
+      foods = create_list(:food, 10)
+      create_list(:meal_food, 15, meal: meals.sample, food: foods.sample)
+  
+      get 'api/v1/meals'
+      result = JSON.parse(response.body)
+
+      expect(result).to be_an(Array)
+      expect(result.count).to eq(3)
+      expect(result.first).to have_key(:"id")
+      expect(result.first).to have_key(:"name")
+      expect(result.first).to have_key(:"foods")
+      expect(result.last[:"foods"]).to be_an(Array)
+      expect(result.last[:"foods"].first).to have_key(:"id")
+      expect(result.last[:"foods"].first).to have_key(:"name")
+      expect(result.last[:"foods"].first).to have_key(:"calories")
+    end
+  end
+end
