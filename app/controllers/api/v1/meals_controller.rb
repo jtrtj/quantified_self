059369a27439022ffaf844724efc03a3369011b9
jtrtj <jt.r@live.com>
2018-10-05 17:06:31 -0600

    write show method for meals controller

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::MealsController < ApplicationController
   def index
     render json: MealsPresenter.new.jsonable_array
   end
+
+  def show
+    meal_presenter = MealPresenter.new(meal_params)
+    render json:   meal_presenter.jsonable_hash,
+           status: meal_presenter.status
+  end
 end
