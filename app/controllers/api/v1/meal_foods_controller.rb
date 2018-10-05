5d8d7d10775d1aaee275a4909009234dc44d4cd7
jtrtj <jt.r@live.com>
2018-10-05 17:44:26 -0600

    write create method on meal foods controller

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::MealFoodsController < ApplicationController
   def create
+    meal_food_creator = MealFoodCreator.new(meal_food_params)
+    render json:   meal_food_creator.message,
+           status: meal_food_creator.status
   end
 end
