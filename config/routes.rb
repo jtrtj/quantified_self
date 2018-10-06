cb5a7627d91b454ae8075fcf8cda60021016887a
jtrtj <jt.r@live.com>
2018-10-06 16:08:23 -0600

    make id params in routes explicit

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/foods', to: 'foods#index'
-      get '/foods/:id', to: 'foods#show'
+      get '/foods/:food_id', to: 'foods#show'
       post '/foods', to: 'foods#create'
-      patch '/foods/:id', to: 'foods#update'
-      delete '/foods/:id', to: 'foods#destroy'
+      patch '/foods/:food_id', to: 'foods#update'
+      delete '/foods/:food_id', to: 'foods#destroy'
 
       get '/meals', to: 'meals#index'
-      get '/meals/:id/foods', to: 'meals#show'
+      get '/meals/:meal_id/foods', to: 'meals#show'
       post '/meals/:meal_id/foods/:food_id', to: 'meal_foods#create'
       delete '/meals/:meal_id/foods/:food_id', to: 'meal_foods#destroy'
 
