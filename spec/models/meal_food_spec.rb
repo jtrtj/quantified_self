f30c803a08eee2283fabf0600ed88012e0650494
jtrtj <jt.r@live.com>
2018-10-06 13:16:40 -0600

    add counter cache to food

--- spec/models/meal_food_spec.rb
+++ spec/models/meal_food_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe MealFood, type: :model do
   context 'validations' do
     it { should belong_to(:meal) } 
-    it { should belong_to(:food).counter_cache(true) }
+    it { should belong_to(:food).counter_cache(:meals_count) }
   end
 end
