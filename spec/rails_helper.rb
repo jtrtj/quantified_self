21932033fd9a400f1f129d8246e45882887132c7
jtrtj <jt.r@live.com>
2018-10-06 16:00:55 -0600

    add bullet to testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  if Bullet.enable?
+    config.before(:each) { Bullet.start_request }
+    config.after(:each)  { Bullet.end_request }
+  end
 end
